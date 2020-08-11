6d4deeddfb06d97cbf9cec866295c032ba7f5971
brinkqiang <brink.qiang@gmail.com>
2020-08-11 14:14:32 +0800

    add

--- cmake/ModuleCompileOptions.cmake
+++ cmake/ModuleCompileOptions.cmake
@@ -195,3 +195,24 @@ macro(ModuleSetCompileOptions)
     ENDIF(CCACHE_FOUND)
   ENDIF ()
 endmacro(ModuleSetCompileOptions)
+
+macro(ModuleSetWinCompilerFlags)
+  IF (WIN32)
+    set(CompilerFlags
+            CMAKE_CXX_FLAGS
+            CMAKE_CXX_FLAGS_DEBUG
+            CMAKE_CXX_FLAGS_RELEASE
+            CMAKE_CXX_FLAGS_RELWITHDEBINFO
+            CMAKE_CXX_FLAGS_MINSIZEREL
+            CMAKE_C_FLAGS
+            CMAKE_C_FLAGS_DEBUG
+            CMAKE_C_FLAGS_RELEASE
+            CMAKE_C_FLAGS_RELWITHDEBINFO
+            CMAKE_C_FLAGS_MINSIZEREL
+            )
+    foreach(CompilerFlag ${CompilerFlags})
+      string(REPLACE "/MD" "/MT" ${CompilerFlag} "${${CompilerFlag}}")
+    endforeach()
+  ENDIF (WIN32)
+endmacro(ModuleSetWinCompilerFlags)
+
---
+++ cmake/replace.bat
@@ -0,0 +1,9 @@
+dmreplacefile --SRCFILE="ModuleImport.cmake" --TPLFILE="ModuleImport.cmake" --PATH="..\thirdparty"
+dmreplacefile --SRCFILE="ModuleImport.cmake" --TPLFILE="ModuleImport.cmake" --PATH="..\src"
+
+dmreplacefile --SRCFILE="ModuleCompileOptions.cmake" --TPLFILE="ModuleCompileOptions.cmake" --PATH="..\thirdparty"
+dmreplacefile --SRCFILE="ModuleCompileOptions.cmake" --TPLFILE="ModuleCompileOptions.cmake" --PATH="..\src"
+
+dmreplacefile --SRCFILE="LuaModule.cmake" --TPLFILE="LuaModule.cmake" --PATH="..\thirdparty"
+dmreplacefile --SRCFILE="LuaModule.cmake" --TPLFILE="LuaModule.cmake" --PATH="..\src"
+PAUSE
